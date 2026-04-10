1ae0b1ab660f27c52118b90c950f0377e288e4ab
Rafael Contieri <155206195+ContiRafa@users.noreply.github.com>
2026-04-10 00:29:57 -0300

    bugs corrigidos

---
+++ Projeto_RJ/modal_Aviso.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_RJ
+{
+    public partial class modalError : Form
+    {
+        public modalError()
+        {
+            InitializeComponent();
+        }
+
+        private void lblNomeMedico_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e) //botao de sim
+        {
+            SessaoUsuario.deslogarEFecharSistema(); //FecharSistema() apenas fecha o sistema para a tela de login /--/ DeslogarUsuario() limpa a sessão e fecha o sistema
+            
+        }
+
+        private void modalError_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) //corrigir aqui para o botão de não, para fechar apenas o modal
+        {
+            
+        }
+    }
+}
